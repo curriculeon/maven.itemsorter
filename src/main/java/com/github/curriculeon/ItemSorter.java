fcbd57c591d1903f61188e45d89e0c2f8ef3fc71
santa-js-html <sanafaris2000@yahoo.com>
2020-01-21 10:46:14 -0500

    Done 1-21-20

--- src/main/java/com/github/curriculeon/ItemSorter.java
+++ src/main/java/com/github/curriculeon/ItemSorter.java
@@ -6,10 +6,28 @@ import java.util.Comparator;
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    private Item[] items;
     public ItemSorter(Item[] items) {
+        this.items = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        Item itemTemp;
+        int i;
+        for(i=0; i < this.items.length-1; i++)
+        {
+                 if ((comparator.compare(this.items[i], this.items[i + 1]) == 1) ||
+                         (comparator.compare(this.items[i], this.items[i + 1]) > 0)) {
+                    itemTemp = this.items[i];
+                    this.items[i] = this.items[i + 1];
+                    this.items[i + 1] = itemTemp;
+                     i=-1;
+                }  else if((comparator.compare(this.items[i], this.items[i + 1]) == -1) ||
+                         (comparator.compare(this.items[i], this.items[i + 1]) == 0) ||
+                         (comparator.compare(this.items[i], this.items[i + 1]) < 0)){
+                     continue;
+                 }
+        }
+        return this.items;
     }
 }
--- src/main/java/com/github/curriculeon/comparators/NameComparator.java
+++ src/main/java/com/github/curriculeon/comparators/NameComparator.java
@@ -1,7 +1,15 @@
 package com.github.curriculeon.comparators;
 
+import com.github.curriculeon.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class NameComparator {
+public class NameComparator implements Comparator<Item> {
+    @Override
+    public int compare(Item item, Item t1) {
+        return item.getName().compareTo(t1.getName());
+    }
 }
